1b96855d8f2c61206731db3c2bb8502c8db8b826
ChoctawChief <dt52hhs@gmail.com>
2022-10-10 15:13:11 -0500

    fix privleges

--- nmap_pingsweep.sh
+++ nmap_pingsweep.sh
@@ -6,5 +6,5 @@ awk '/Host/ {print $2}' ip_sweep >ip_scope
 cat ip_scope
 read -p "Press one to iniate portscan on enumerated targets  Press two to quit" opt
 if [ "$opt" == "1" ]; then
-        nmap -O -sV -oG port_scan  -iL ip_scope
+       sudo nmap -O -sV -oG port_scan  -iL ip_scope
 fi

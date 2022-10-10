aba25466774b66ab15eaac9e4a1a608ee8506ccd
ChoctawChief <dt52hhs@gmail.com>
2022-10-10 14:30:54 -0500

    add awk

--- nmap_pingsweep.sh
+++ nmap_pingsweep.sh
@@ -2,7 +2,8 @@
  read -p "what is your target ?" ip 
 
 nmap -sn  $ip   -oG ip_sweep
- 
-read -p "Press one to iniate portscan on enumerated targets Press two to quit" $opt	
+awk '/Host/ {print $2}' ip_sweep =ip_scope
+
+read -p "Press one to iniate portscan on enumerated targets $ip_scope Press two to quit" $opt	
 if $opt = 1 ;then
-	
+	nmap -sc -sv -oG port_scan

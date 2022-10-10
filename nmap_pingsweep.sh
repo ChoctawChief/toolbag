927ffdcff17b6efa1a5d60bc57eb7242dd96069e
ChoctawChief <dt52hhs@gmail.com>
2022-10-10 15:11:07 -0500

    bughunting

--- nmap_pingsweep.sh
+++ nmap_pingsweep.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
- read -p "what is your target ?" ip 
+ read -p "what is your target ?" ip
 
-nmap -sn  $ip   -oG ip_sweep
-awk '/Host/ {print $2}' ip_sweep =ip_scope
-
-read -p "Press one to iniate portscan on enumerated targets $ip_scope Press two to quit" $opt	
-if $opt = 1 ;then
-	nmap -sc -sv -oG port_scan
+nmap -sn  "$ip"   -oG ip_sweep
+awk '/Host/ {print $2}' ip_sweep >ip_scope
+cat ip_scope
+read -p "Press one to iniate portscan on enumerated targets  Press two to quit" opt
+if [ "$opt" == "1" ]; then
+        nmap -O -sV -oG port_scan  -iL ip_scope
+fi

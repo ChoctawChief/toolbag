d3718a8a524be151d5284f9d79f185757bd6c3d2
ChoctawChief <dt52hhs@gmail.com>
2022-10-10 12:55:26 -0500

    fix

--- nmap_pingsweep.sh
+++ nmap_pingsweep.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
- read -p "what is your target ? " $ip 
-nmap -sn $ip | tee ip_sweep
+ read -p "what is your target ?" ip 
 
+nmap -sn  $ip   -oG ip_sweep
+ 
+read -p "Press one to iniate portscan on enumerated targets Press two to quit" $opt	
+if $opt = 1 ;then
+	

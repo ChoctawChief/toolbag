c9865c39f4a2f86602da1233257fa21f1129a24c
ChoctawChief <dt52hhs@gmail.com>
2022-10-12 23:56:33 -0500

    cleaning up

--- enum/proxychainnmap.sh
+++ enum/proxychainnmap.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-read -m"What Is Your Target? " IP_Address
+read -p "What Is Your Target? " IP_Address
 Sudo proxychains -q nmap -oG proxyscan --top-ports=100 -sT -sV -sC  -v --open  -Pn $IP_Address
+cat proxyscan

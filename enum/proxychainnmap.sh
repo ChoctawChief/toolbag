8af4f4d3e7fa14e2e060fc07053465c2ad9fee5d
ChoctawChief <dt52hhs@gmail.com>
2022-10-13 10:11:05 -0500

    bug squash

--- enum/proxychainnmap.sh
+++ enum/proxychainnmap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 read -p "What Is Your Target? " IP_Address
-Sudo proxychains -q nmap -oG proxyscan --top-ports=100 -sT -sV -sC  -v --open  -Pn $IP_Address
+sudo proxychains -q nmap -oG proxyscan --top-ports=100 -sT -sV -sC  -v --open  -Pn $IP_Address
 cat proxyscan
